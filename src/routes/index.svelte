f319504e72ddab1cfeb6daf05f72a33d084916dd
Monarch Wadia <monarch@mintbean.io>
2021-08-26 19:12:23 -0400

    little feedback loop going

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+let happiness = 100;
+let cash = 10_00;
+</script>
+
+<h1>Bootcamp Card Game</h1>
+<div class="wrapper">
+  <button on:click={() => happiness++}>Happiness++</button>
+  <button on:click={() => cash++}>Cash++</button>
+  {happiness}
+  {cash}
+</div>
+<style>
+.wrapper {
+  /* background-color: black; */
+  display: flex;
+  flex-direction: column;
+}
+</style>
